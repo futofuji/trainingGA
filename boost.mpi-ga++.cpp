77ee4248cd3ba08e6a9b56ba61627e2aca6eb4a8
mikiya fujii <mikiya.fujii@gmail.com>
2013-01-01 06:45:50 +0900

    An instance of the GA is created as boost::shared_ptr.

--- boost.mpi-ga++.cpp
+++ boost.mpi-ga++.cpp
@@ -1,5 +1,6 @@
 #include <boost/mpi/environment.hpp>
 #include <boost/mpi/communicator.hpp>
+#include <boost/shared_ptr.hpp>
 #include <iostream>
 #include "ga++.h"
 
@@ -10,6 +11,11 @@ int main(int argc, char* argv[])
    GA::GAServices gaServices;
    std::cout << "I am process " << world.rank() << " of " << world.size() << ". " 
              << "My node-id is " << gaServices.clusterNodeid() << std::endl;
+
+   int ndim=1;
+   int dims[]={5};
+   char* arrayName = "array_name";
+   boost::shared_ptr<GA::GlobalArray> ptrGA(new GA::GlobalArray(MT_C_DBL, ndim, dims, arrayName, NULL));
    GA::Terminate();
    return 0;
 }
